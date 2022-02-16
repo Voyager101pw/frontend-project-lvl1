8d5add1a7d3d9e4b01b443fd3fe2e91dc9e18060
tester <tester@gamil.coc>
2022-02-16 05:42:36 +0500

    Changed code organization

--- src/games/calc-logic.js
+++ src/games/calc-logic.js
@@ -1,25 +1,27 @@
 import startQuiz from '../index.js';
 import getRandomNumber from '../getRandomNumber.js';
 
-export default () => {
-  const generationQA = () => {
-    const operation = ['+', '-', '*'][getRandomNumber(0, 2)];
-    const [num1, num2] = [getRandomNumber(0, 10), getRandomNumber(0, 10)];
-    const question = `${num1} ${operation} ${num2}`;
-    let correctAnswer = null;
-    switch (operation) {
-      case '+':
-        correctAnswer = String(num1 + num2);
-        return [question, correctAnswer];
-      case '-':
-        correctAnswer = String(num1 - num2);
-        return [question, correctAnswer];
-      default:
-        correctAnswer = String(num1 * num2);
-        return [question, correctAnswer];
-    }
-  };
+const generationQA = () => {
+  const operation = ['+', '-', '*'][getRandomNumber(0, 2)];
+  const [num1, num2] = [getRandomNumber(1, 10), getRandomNumber(1, 10)];
 
-  const gameDescription = 'What is the result of the expression?';
-  startQuiz(gameDescription, generationQA);
+  const question = `${num1} ${operation} ${num2}`;
+  let correctAnswer = null;
+
+  switch (operation) {
+    case '+':
+      correctAnswer = String(num1 + num2);
+      return [question, correctAnswer];
+    case '-':
+      correctAnswer = String(num1 - num2);
+      return [question, correctAnswer];
+    default:
+      correctAnswer = String(num1 * num2);
+      return [question, correctAnswer];
+  }
 };
+
+const gameDescription = 'What is the result of the expression?';
+const startGame = () => startQuiz(gameDescription, generationQA);
+
+export default startGame;
--- src/games/even-logic.js
+++ src/games/even-logic.js
@@ -1,14 +1,14 @@
 import startQuiz from '../index.js';
 import getRandomNumber from '../getRandomNumber.js';
 
-export default () => {
-  const generationQA = () => {
-    const number = getRandomNumber(0, 100);
-    const question = number;
-    const correctAnswer = number % 2 === 0 ? 'yes' : 'no';
-    return [question, correctAnswer];
-  };
-
-  const gameDescription = 'Answer "yes" if the number is even, otherwise answer "no".';
-  startQuiz(gameDescription, generationQA);
+const generationQA = () => {
+  const number = getRandomNumber(0, 100);
+  const question = number;
+  const correctAnswer = number % 2 === 0 ? 'yes' : 'no';
+  return [question, correctAnswer];
 };
+
+const gameDescription = 'Answer "yes" if the number is even, otherwise answer "no".';
+const startGame = () => startQuiz(gameDescription, generationQA);
+
+export default startGame;
